97f4832daa99f79fc730f80c0914036e868deef0
Nelson Perez <neeperez@ucsc.edu>
2018-10-19 15:34:03 -0700

    Basic paths on the website are set up

--- DS-Web-App/src/App.js
+++ DS-Web-App/src/App.js
@@ -10,7 +10,11 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route path="/" component = {Home} exact strict />
-          <Route path = "/9-10" exact component = {DiningHall} />
+          <Route path = "/9-10" exact render = {() => <DiningHall name="9/10"/>} />
+          <Route path = "/cowell-stevenson" exact render = {() => <DiningHall name="Cowell/Stevenson"/>} />
+          <Route path = "/crown-merrill" exact render = {() => <DiningHall name="Crown/Merill"/>} />
+          <Route path = "/porter-kresge" exact render = {() => <DiningHall name="Porter/Kresge"/>} />
+          <Route path = "/rcc-oakes" exact render = {() => <DiningHall name="Rachel Carson/Oakes"/>} />
         </Switch>
       </BrowserRouter>
     );
--- DS-Web-App/src/components/DiningHall.js
+++ DS-Web-App/src/components/DiningHall.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 
+//Now all we need to do here is grab the information from the server on what the dining halls are serving
 export class DiningHall extends React.Component {
 	render(){
 		return(
 			<div>
-				<h1>Dining Hall!</h1>
+				<h1>{this.props.name}</h1>
 				<h1>
 					<NavLink to="/" exact>
 						<button>Home</button>
