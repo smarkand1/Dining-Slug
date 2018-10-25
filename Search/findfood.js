7df73f0e328f75d77e4f0e7dae0f61dd5b0f76f1
ALKW <30512088+ALKW@users.noreply.github.com>
2018-10-25 16:55:23 -0700

    Added Basic Search Function

---
+++ Search/findfood.js
@@ -0,0 +1,33 @@
+function findfood() {
+    var input; //Assign this to read input from seach bar
+    var data = ["chicken", "cheese", "meat", "pasta", "Sausage"]; // This value will be static and always the same read from the 
+    var output = []; // These values should display in a list style under the search bar
+    input = "au"; //fill with search bar data
+    output = filterItems(data, input)
+    console.log(output) //Output the data
+}
+
+function filterItems(searchArray, query) {
+    /*
+    filters an array based on items
+    Arguements:
+        searchArray - (type = array of strings)
+        query - string to look for within other strings
+    Returns:
+        Array - (type= array of strings) - array of strings that contains query
+    Raises:
+        None
+    */
+    return searchArray.filter(function(el) {
+        return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
+    });
+}
+
+//Call find food whenever the search bar is interacted with. 
+/*
+Interactions: 
+    Typing a letter
+    Deleting a letter
+    Clicking the seach bar
+Clicking off the search bar should close the list
+*/
