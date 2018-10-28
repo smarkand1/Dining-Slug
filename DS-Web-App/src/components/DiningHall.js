ccc4281cf63a5b91b5ea7aabadf9dbf25511b49a
cborrero <cborrero@ucsc.edu>
2018-10-27 19:44:15 -0700

    Lots of styling

--- DS-Web-App/src/components/DiningHall.js
+++ DS-Web-App/src/components/DiningHall.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import { Listhome } from './../Listhome';
+
 
 //Now all we need to do here is grab the information from the server on what the dining halls are serving
 export class DiningHall extends React.Component {
 	render(){
 		return(
-			<div>
-				<h1>{this.props.name}</h1>
-				<h1>
-					<NavLink to="/" exact>
-						<button>Home</button>
-					</NavLink>
-				</h1>
-			</div>
+
+			<div className="App">
+			<header className="App-header">
+			  <div className="title">
+			    {this.props.name}
+				<img src ={require('./slug.png')}/>
+			  </div>
+			  <div className="gradient"></div>
+			  <Listhome/>
+			</header>
+		  </div>
 		);
 	}
 }
