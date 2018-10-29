a6ef4e85433406477700a0cb9d32a9d9410663e3
jloyola126 <44045456+jloyola126@users.noreply.github.com>
2018-10-29 15:34:38 -0700

    Create menu-ui.js
    Needs to be re-done to match Alex's txt file

---
+++ DS-Web-App/src/components/menu-ui.js
@@ -0,0 +1,10 @@
+//This module is our data.txt and will be used for extraction for each 
+//dining hall page
+//The menu array should be in a nested for loop so it can accomodate Alex's file
+module.exports = [{
+	Title: ["Colleges Nine & Ten Menu"],
+	Date: ["Menus for Wednesday, October 24, 2018"],
+	Hours: ["Hours go here"],
+  Menu: ["Breakfast", "Lunch", "Dinner"],
+  Food: []
+}]
