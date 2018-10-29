9208e5908ab481769a082096aa648bebeb0184f1
jloyola126 <44045456+jloyola126@users.noreply.github.com>
2018-10-29 15:36:49 -0700

    Updated NineTen which is Menu.js
    This is where the for loop needs to be implemented in recipe.Menu.map  to display columns.
    Menu is Breakfast,Lunch, Dinner, and Food is the list of food items.

--- DS-Web-App/src/components/NineTen.js
+++ DS-Web-App/src/components/NineTen.js
@@ -1,1 +1,67 @@
-import React from 'react';
+//Listing page for Crown, THIS IS called in DiningHall.js
+import React from 'react'
+     
+export class NineTen extends React.Component { 
+        constructor(props){
+        super(props);
+           this.state={ recipes :[] } 
+        }
+    
+       
+        componentWillMount(){
+          this.setState({
+            recipes : require('./menu-ui')
+          });
+        } 
+        render() {
+         return (
+      
+          <div className="App">
+             <h2>Welcome to the 9/10 Listing!</h2>
+             <dl>
+             {this.state.recipes.map(recipe => {
+                 return ( <div key={recipe.Menu}>
+                          <div key={recipe.Hours}></div>
+                          <div key={recipe.Title}></div>
+
+                    <ul> {recipe.Title.map(function(Title,index)
+                    {
+                      return <tab key = {index}>{Title}</tab>
+                    })}
+                    </ul>     
+                    <ul> {recipe.Hours.map(function(Hours,index)
+                    {
+                      return <tab key = {index}>{Hours}</tab>
+                    })}
+                    </ul>
+                    <ul> {recipe.Menu.map(function(Menu,index)
+                    {
+                      return  <tab key = {index}>{Menu}</tab>
+                    }
+                    )}
+                    </ul>
+                     <br></br> 
+                     <dd>{recipe.Food.join(',')}</dd>
+                     <br></br> 
+                     <dt>{recipe.Menu[2]}</dt>
+                     <br></br> 
+                     <dd>{recipe.Food.join(',')}</dd>
+                     <br></br> 
+                     <dt>{recipe.Menu[3]}</dt>
+                     <br></br> 
+                     <dd>{recipe.Food.join(',')}</dd>
+                     <br></br> 
+                     <hr></hr>
+                    </div>
+                   )
+                 })
+             }
+          </dl>
+          </div>
+        );
+     }
+    }
+    export default NineTen;
+    
+
+
