a18949366e8f2cfc35343ce8ddba77754126f4ed
jloyola126 <44045456+jloyola126@users.noreply.github.com>
2018-10-29 19:16:50 -0700

    Update List.js

--- DS-Web-App/src/List.js
+++ DS-Web-App/src/List.js
@@ -2,40 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch, NavLink} from 'react-router-dom';
 import { DiningHall } from './components/DiningHall';
+import './List.css';
+
 
 export class List extends React.Component{
 	render() {
 		return (
 			<div>
-				<h1>List!</h1>
 				<ul>
 					<li>
 						<NavLink to = "/9-10" exact>
-							<button>9/10</button>
+							<button class = "listButton">9/10</button>
 						</NavLink>
 					</li>
 					<li>
 						<NavLink to = "/cowell-stevenson" exact>
-							<button>Cowell/Stevenson</button>
+							<button class = "listButton">Cowell/Stevenson</button>
 						</NavLink>
 					</li>
 					<li>
 					<NavLink to = "crown-merrill" exact>
-							<button>Crown/Merrill</button>
+							<button class = "listButton">Crown/Merrill</button>
 						</NavLink>
 					</li>
 					<li>
 						<NavLink to = "/porter-kresge" exact>
-							<button>Porter/Kresge</button>
+							<button class = "listButton">Porter/Kresge</button>
 						</NavLink>
 					</li>
 					<li>
 						<NavLink to = "/rcc-oakes" exact>
-							<button>Rachel Carson/Oakes</button>
+							<button class = "listButton">Rachel Carson/Oakes</button>
 						</NavLink>
 					</li>	
 				</ul>
 			</div>
+
 		);
 	}
 }
