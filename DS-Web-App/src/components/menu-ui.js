81bc7b7150277db21200a368ce6ffd41de1fa5b8
jloyola126 <44045456+jloyola126@users.noreply.github.com>
2018-10-29 19:13:58 -0700

    Create menu-ui.js
    JS file in place of data.txt needs formatting

---
+++ DS-Web-App/src/components/menu-ui.js
@@ -0,0 +1,9 @@
+//This module is our data.txt and will be used for extraction for each 
+//dining hall page
+module.exports = [{
+	Title: ["Colleges Nine & Ten Menu"],
+	Date: ["Menus for Wednesday, October 24, 2018"],
+	Hours: ["Hours go here"],
+    Menu: ["Breakfast", "Lunch", "Dinner"],
+    Food: []
+}]
