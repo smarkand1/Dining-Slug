269acf2be3a1aac8cfa4c5ecbf95ebd17e0ab70e
jloyola126 <44045456+jloyola126@users.noreply.github.com>
2018-10-29 19:12:25 -0700

    Update DiningHall.js
    Calls NineTen

--- DS-Web-App/src/components/DiningHall.js
+++ DS-Web-App/src/components/DiningHall.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import { Listhome } from './../Listhome';
+import {NineTen} from './NineTen';
 
 //Now all we need to do here is grab the information from the server on what the dining halls are serving
 export class DiningHall extends React.Component {
+	//Currently I'm just rendering a static JSON object for every dininghall.
+	//Im testing it using the RCC dining hall to get a feel for how it works
 	render(){
 		return(
-			<div>
-				<h1>{this.props.name}</h1>
-				<h1>
-					<NavLink to="/" exact>
-						<button>Home</button>
-					</NavLink>
-				</h1>
-			</div>
+			<div className="App">
+				<header className="App-header">
+				  <div className="title">
+				    {this.props.name}
+					<img src ={require('./slug.png')}/>
+				  </div>
+				  <div className="gradient"></div>
+				  <Listhome/>
+				  <NineTen/>
+				  
+				</header>
+		  </div>
 		);
 	}
 }
