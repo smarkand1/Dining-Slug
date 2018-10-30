fcbfb65e2a4bc4e964f3e1622e2401511b6813c9
jloyola126 <44045456+jloyola126@users.noreply.github.com>
2018-10-29 19:17:50 -0700

    Update List.js
    Includes the styling portion of our website

--- DS-Web-App/src/List.js
+++ DS-Web-App/src/List.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch, NavLink} from 'react-router-dom';
 import { DiningHall } from './components/DiningHall';
 import './List.css';
