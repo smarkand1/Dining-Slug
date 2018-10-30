409d7c40462ea06036dc7014b0e1af4d424efaa4
jloyola126 <44045456+jloyola126@users.noreply.github.com>
2018-10-29 19:22:04 -0700

    Create Listhome

---
+++ DS-Web-App/src/Listhome.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, NavLink} from 'react-router-dom';
+import { DiningHall } from './components/DiningHall';
+import './List.css';
+
+export class Listhome extends React.Component{
+	render() {
+		return (
+			<div>
+				<ul>
+					<li>
+						<NavLink to = "/" exact>
+							<button class = "listButton">Home</button>
+						</NavLink>
+					</li>	
+					<li>
+						<NavLink to = "/9-10" exact>
+							<button class = "listButton">9/10</button>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to = "/cowell-stevenson" exact>
+							<button class = "listButton">Cowell/Stevenson</button>
+						</NavLink>
+					</li>
+					<li>
+					<NavLink to = "crown-merrill" exact>
+							<button class = "listButton">Crown/Merrill</button>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to = "/porter-kresge" exact>
+							<button class = "listButton">Porter/Kresge</button>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to = "/rcc-oakes" exact>
+							<button class = "listButton">Rachel Carson/Oakes</button>
+						</NavLink>
+					</li>	
+				</ul>
+			</div>
+
+		);
+	}
+}
