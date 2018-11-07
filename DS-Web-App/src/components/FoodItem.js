eb05770b059b032d801523c500504c12e8757d0c
Nelson Perez <neeperez@ucsc.edu>
2018-11-07 14:36:30 -0800

    Added functionality for rating, used npm install react-ratings-declarative for the rating system

--- DS-Web-App/src/components/FoodItem.js
+++ DS-Web-App/src/components/FoodItem.js
@@ -5,17 +5,45 @@
  */
 
 import React from 'react';
-import './../List.css';
+import './FoodItem.css';
+import Ratings from 'react-ratings-declarative';
 
 export class FoodItem extends React.Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {rating: 0};
+        this.changeRating = this.changeRating.bind(this);
+    }
+
+    changeRating(newRating){
+        this.setState({
+            rating: newRating
+        });
     }
 
     render() {
         return(
-            <button className = "listButton">{this.props.itemName}</button>
+            <div>
+                <button className = "listFood">
+                    {this.props.itemName}
+                    <Ratings  
+                        rating = {this.state.rating}
+                        widgetRatedColors = 'rgb(204, 204, 0)'
+                        widgetEmptyColors = 'rgb(203, 211, 227)'
+                        widgetHoverColors = 'rgb(255, 255, 0)'
+                        changeRating = {this.changeRating}
+                        widgetDimensions = '20px'
+                    >
+                        <Ratings.Widget />
+                        <Ratings.Widget />
+                        <Ratings.Widget />
+                        <Ratings.Widget />
+                        <Ratings.Widget />
+                    </Ratings>
+                </button>
+                
+            </div>
+               
         );
     }
 }
