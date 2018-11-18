01af8f1051fd28f27c82bf519ab9511a9eead1ac
borreroc <cborrero@ucsc.edu>
2018-11-17 17:27:31 -0800

    Changed php script for javascript communication

--- DS-Web-App/databasehelper.php
+++ DS-Web-App/databasehelper.php
@@ -1,28 +1,26 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$db = "diningslug";
+	$Food = _POST('Food');
+	$Rating = _POST('Rating');
+	$Reviews = _POST('Reviews');
+	echo $Food;
+	echo $Rating;
+	echo $Reviews;
+	//$servername = "localhost";
+	//$username = "root";
+	//$password = "";
+	//$db = "diningslug";
 
 	//Create connection
-	$conn = mysqli_connect($servername, $username, $password, $db) or die("Connection Failed");
-	echo "Connected Successfully";
-	//Execute the webscraper 
-	$result = exec("scraper.py C:\wamp64\www\Dining-Slug\Webscraper");
-	//Extract result of webscraper from .txt file
-	$filename = "data.txt";
-	$data = file_get_contents($filename);
-	$dharray = json_decode($data, true);
-	foreach($dharray as $row){
-		$sql = "INSERT INTO dininghalldata (ID, DiningHall, Breakfast, Lunch, Dinner, LateNight)
-		VALUES ('".$row["ID"]."', '".$row["DiningHall"]."', '".$row["Breakfast"]."', '".$row["Lunch"]"', '".$row["Dinner"]."', '".$row["LateNight"]."')";
-	}
-
-	if ($conn->query($sql) === TRUE) {
-    		echo "New record created successfully";
-	} else {
-    		echo "Error: " . $sql . "<br>" . $conn->error;
-	}
+	//$conn = mysqli_connect($servername, $username, $password, $db) or die("Connection Failed");
+	//echo "Connected Successfully";
+	
+	//$sql = "INSERT INTO 
+	//if ($conn->query($sql) === TRUE) {
+    		//echo "New record created successfully";
+	//} else {
+    		//echo "Error: " . $sql . "<br>" . $conn->error;
+	//}
+	
 
 		
 
