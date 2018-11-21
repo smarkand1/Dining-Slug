1e1df2049d925c6ec3028ce484bf5ca54eff44b4
Nelson Perez <neeperez@ucsc.edu>
2018-11-21 09:16:09 -0800

    Attempt to restructure backend of application

--- DS-Web-App/server/index.js
+++ DS-Web-App/server/index.js
@@ -1,16 +1,21 @@
-const express = require('express');
+const express = require('../client/node_modules/express');
 const path = require('path');
+const mysql = require('../client/node_modules/mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'user'
+})
 
 const app = express();
 
 //serve the state files from the React app
-app.use(express.static(path.join(__dirname, '/../build')));
+app.use(express.static(path.join(__dirname, '/../client/build')));
 
 //An api endpoint that returna a short list of items
 //This may be where we put the sql stuff
 
 app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname+'/../build/index.html'));
+    res.sendFile(path.join(__dirname+ '/../client/build/index.html'));
     console.log("Refresh");
 });
 
