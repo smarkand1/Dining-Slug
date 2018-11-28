4430e5bbc78f3f33dc49f85c97318a5875a10ea5
Alexander Widmann <widmannalex1498@gmail.com>
2018-11-27 17:52:01 -0800

    Fixed Comment

--- DS-Web-App/src/components/FoodPage.js
+++ DS-Web-App/src/components/FoodPage.js
@@ -16,8 +16,7 @@ export class FoodPage extends React.Component {
     //this.state["preferences"] is an array of vegan, soy, etc. items
     //This array could be empty
 
-    //this.state["preferences"] is an array of vegan, soy, etc. items
-    //This array could be empty
+    //this.state["url"] is the url
     
     //this.state["diningHall"] is an array of the dining halls the item is serving in
     //This array always has at least 1 element in it 
