4f91000525e1cca69ffc2fa11c66b56e60c49fea
borreroc <cborrero@ucsc.edu>
2018-11-30 13:25:21 -0800

    Server push

---
+++ build/precache-manifest.ca6a57d043f22446c3f691c6789cabbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f9d78146e0f8ec048504",
+    "url": "/static/js/main.f9d78146.chunk.js"
+  },
+  {
+    "revision": "328f4e302ed09e04f854",
+    "url": "/static/js/1.328f4e30.chunk.js"
+  },
+  {
+    "revision": "f9d78146e0f8ec048504",
+    "url": "/static/css/main.996a5867.chunk.css"
+  },
+  {
+    "revision": "93fd1c2ee43590e0edc17bd7b7f83bb4",
+    "url": "/index.html"
+  }
+];
